aa0fbd4b701df10316aa674ceb7a04ba04f105ef
José <trujillohenaresj@gmail.com>
2022-11-16 20:46:15 +0100

    feat: :card_file_box: adding mongodb connection config
    The database is MongoDB. I prepare a docker-compose.yml file to run the database on a container.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes"
+import db from "./config/mongo"
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,5 +10,6 @@ app.use(cors({
     origin: "*"
 }));
 app.use(router);
+db().then(() => console.log("Database connected"));
 app.listen(PORT, () => console.log("API running in port " + PORT));
 
