09b9ee888acdfc537c8de2483f5b63af2d847644
José <trujillohenaresj@gmail.com>
2022-11-17 18:06:32 +0100

    feat: :loud_sound: basic log middleware

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,12 +1,13 @@
 import { Request, Response, Router } from "express";
 import { deleteItem, getItem, getItems, postItem, updateItem } from "../controllers/item";
+import { logMiddleware } from "../middleware/log";
 
 const router= Router();
 /**
  * localhost:3002/items [GET]
  */
 router.post("/", postItem);
-router.get("/", getItems);
+router.get("/", logMiddleware, getItems);
 router.get("/:id", getItem);
 router.put("/:id", updateItem);
 router.delete("/:id", deleteItem);
